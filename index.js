43ebdcf604294fc9d12c4abc2e06e193d0862d92
Sander Hahn <sander.hahn@kpn.com>
2023-02-11 00:38:34 +0100

    fix order so trackTrace is correctly associated

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const appInsights = require('applicationinsights');
 const express = require('express');
 
 dotenv.config();
-appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
-const appInsightsClient = appInsights.defaultClient;
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+
+appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
+const appInsightsClient = appInsights.defaultClient;
 
 app.get('/', (req, res) => {
     appInsightsClient.trackTrace({
